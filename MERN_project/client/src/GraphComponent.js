ce44cc790b40e4929109baf3ea8ce9febc139a1a
Piyush Daulatkar <141228456+PiyushDaulatkar@users.noreply.github.com>
2024-04-17 19:51:40 +0530

    Update GraphComponent.js

--- MERN_project/client/src/GraphComponent.js
+++ MERN_project/client/src/GraphComponent.js
@@ -1,17 +1,6 @@
 import React from "react";
 import { Chart } from "react-google-charts";
 
-
-// export const data = [
-//   ["Element", "Density", { role: "style" }],
-//   ["Copper", 8.94, "#b87333"], // RGB value
-//   ["Silver", 10.49, "silver"], // English color name
-//   ["Gold", 19.3, "gold"],
-//   ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
-// ];
-
-
-
 export function Graph(props) {
 
 const options = {
