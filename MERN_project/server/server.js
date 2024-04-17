c77d685593c81f03599868387e08ea8241c73563
Piyush Daulatkar <piyushdaulatkar03@gmail.com>
2024-04-17 19:53:48 +0530

    updated project

--- MERN_project/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/api", (req, resp) => {
-    resp.json({"users": ["one","two","three"]})
-})
-
-app.listen(5000, () => {console.log("Server started on port 5000")})
